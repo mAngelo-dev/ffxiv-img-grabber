b618471ba52753389da06e9fbcf1b340b9a4fb5e
Miguel Angelo <m.angelodev@gmail.com>
2025-11-28 19:09:35 -0300

    Add body image

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,27 @@ async function getProfileWolPictureUrl(url: string) : Promise<string> {
   const htmltext = await htmldata.text()
 
   const $ = load(htmltext)
-  const profileWolPictureUrl = $(".frame__chara__face img").attr("src")
+  const profileWolPictureUrl : string | undefined = $(".frame__chara__face img").attr("src")
 
   if (!profileWolPictureUrl) {
-    throw new Error("Profile picture URL not found, check your URL")
+    throw new Error("WOL profile picture URL not found, check your URL")
   }
 
   return profileWolPictureUrl;
 }
+
+async function getWolBodyPictureUrl(url: string) : Promise<string> {
+  const htmldata = await fetch(url)
+  const htmltext = await htmldata.text()
+
+  const $ = load(htmltext)
+  const wolBodyPictureUrl : string | undefined = $(".character__detail__image img").attr("src")
+
+  if (!wolBodyPictureUrl) {
+    throw new Error("WOL body picture URL not found, check your URL")
+  }
+
+  return wolBodyPictureUrl;
+}
+
+export { getProfileWolPictureUrl, getWolBodyPictureUrl }
